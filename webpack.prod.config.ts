6e679827db9e347f4c77769c90e2f975e0a0902b
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-01-24 00:55:37 +0700

    add prod config

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -1,25 +1,20 @@
-import path from "path"
-import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin, container } from "webpack"
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
-import HtmlWebpackPlugin from "html-webpack-plugin"
-import ESLintPlugin from "eslint-webpack-plugin"
+import path from "path";
+import { Configuration, container } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 const deps = require('./package.json').dependencies
 
 const ModuleFederationPlugin = container.ModuleFederationPlugin
 
-interface Configuration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
-}
-
 const config: Configuration = {
-  optimization: {
-    splitChunks: false
-  },
-  mode: "development",
-  output: {
-    publicPath: "/",
-  },
+  mode: "production",
   entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "https://MuhammadChandra19.github.io/animeql-container/",
+  },
   module: {
     rules: [
       {
@@ -45,15 +40,15 @@ const config: Configuration = {
     new HtmlWebpackPlugin({
       template: "build/index.html",
     }),
-    new HotModuleReplacementPlugin(),
     new ESLintPlugin({
       extensions: ["js", "jsx", "ts", "tsx"],
     }),
+    new CleanWebpackPlugin(),
     new ModuleFederationPlugin({
       name: 'animeql_container',
       filename: "remoteEntry.js",
       remotes: {
-        'AnimeListRemote': "AnimeListRemote@http://localhost:4001/remoteEntry.js",
+        'AnimeListRemote': "AnimeListRemote@https://MuhammadChandra19.github.io/animeql-list/remoteEntry.js",
       },
       shared: {
         react: { 
@@ -73,18 +68,8 @@ const config: Configuration = {
         },
       },
     })
+    // new BundleAnalyzerPlugin()
   ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: path.join(__dirname, "build"),
-    historyApiFallback: true,
-    port: 4000,
-    open: true,
-    hot: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  },
 };
 
 export default config
