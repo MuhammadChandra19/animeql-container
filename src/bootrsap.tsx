d929a081a5ed74331305aaee883f14af1fff8557
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-01-28 15:43:51 +0700

    implement react lazy

--- src/bootrsap.tsx
+++ src/bootrsap.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client"
 import { Container, Layout } from './Views/Styles/LayoutStyle'
 import { GlobalStyles } from 'twin.macro'
 import { css } from '@emotion/react'
-import AnimeList from 'AnimeListRemote/AnimeList'
-import TopAnimeList from 'TopAnimeListRemote/TopAnimeList'
+// import AnimeList from 'AnimeListRemote/AnimeList'
+// import TopAnimeList from 'TopAnimeListRemote/TopAnimeList'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const AnimeList = React.lazy(() => import('AnimeListRemote/AnimeList'))
+const TopAnimeList = React.lazy(() => import('TopAnimeListRemote/TopAnimeList'))
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
